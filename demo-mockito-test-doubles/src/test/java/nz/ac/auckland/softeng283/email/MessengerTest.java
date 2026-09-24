57b3250ed1242fa95945f60a0f50364fefae54cb
fancile <fancile23219@gmail.com>
2026-09-24 12:08:12 +1200

    do demos

--- demo-mockito-test-doubles/src/test/java/nz/ac/auckland/softeng283/email/MessengerTest.java
+++ demo-mockito-test-doubles/src/test/java/nz/ac/auckland/softeng283/email/MessengerTest.java
@@ -1,14 +1,31 @@
 package nz.ac.auckland.softeng283.email;
 
+import static org.mockito.ArgumentMatchers.anyString;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class MessengerTest {
 
   @Test
-  @Disabled("Replace this scaffold with Mockito tests")
+  // @Disabled("Replace this scaffold with Mockito tests")
   public void test() {
     // Create mocks for Client, Template, TemplateEngine, and MailServer.
     // Configure the prepared message, call Messenger.sendMessage, and verify interactions.
+    MailServer mailServer = Mockito.mock(MailServer.class);
+    Template template = Mockito.mock(Template.class);
+    TemplateEngine templateEngine = Mockito.mock(TemplateEngine.class);
+    Client client = Mockito.mock(Client.class);
+
+    Messenger messenger = new Messenger(mailServer, templateEngine);
+    //STUB
+    Mockito.when(templateEngine.prepareMessage(template, client)).thenReturn("Hello, World!");
+    Mockito.when(client.getEmail()).thenReturn("example@kk.com");
+    //ACT
+    messenger.sendMessage(client, template);
+
+    //assert
+    Mockito.verify(mailServer).send("example@kk.com", "Hello, World!");
   }
 }
